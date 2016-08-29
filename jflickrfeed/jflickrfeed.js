3c66e08ffdd17734129a9847cee750986f851fb5
ENTPower <csmit186@gmail.com>
2016-08-28 17:35:30 -0700

    changed instances of http to https
    Resolving browser security block for feed prototype config page.

--- jflickrfeed/jflickrfeed.js
+++ jflickrfeed/jflickrfeed.js
@@ -1,7 +1,7 @@
 /*
 * Copyright (C) 2009 Joel Sutherland
 * Licenced under the MIT license
-* http://www.newmediacampaigns.com/page/jquery-flickr-plugin
+* https://www.newmediacampaigns.com/page/jquery-flickr-plugin
 *
 * Available tags for templates:
 * title, link, date_taken, description, published, author, author_id, tags, image*
@@ -9,7 +9,7 @@
 (function($) {
 	$.fn.jflickrfeed = function(settings, callback) {
 		settings = $.extend(true, {
-			flickrbase: 'http://api.flickr.com/services/feeds/',
+			flickrbase: 'https://api.flickr.com/services/feeds/',
 			feedapi: 'photos_public.gne',
 			limit: 20,
 			qstrings: {
@@ -52,7 +52,7 @@
 						}
 						
 						// Add Image Sizes
-						// http://www.flickr.com/services/api/misc.urls.html
+						// https://www.flickr.com/services/api/misc.urls.html
 						item['image_s'] = item.media.m.replace('_m', '_s');
 						item['image_t'] = item.media.m.replace('_m', '_t');
 						item['image_m'] = item.media.m.replace('_m', '_m');
